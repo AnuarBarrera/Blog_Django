a7bc906bcd631c3beab78ddc6a441aed400eaaeb
AnuarBarrera <contacto.neia@gmail.com>
2025-02-17 20:05:36 -0600

    i agreed license and notice copyrigth

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,3 +1,17 @@
+/*
+  Copyright [2005] [anuar eduardo barrera yeeben]
+ 
+ Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+ 
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and limitations under the License.
+*/
+
 // Variables
 const titulo = "Bienvenido a mi Blog";
 const parrafoIntroductorio = "Bienvenido a mi CV en línea. Soy Anuar Barrera, un desarrollador apasionado por crear soluciones tecnológicas innovadoras.Te invito a consultar mi historia laboral y mi portafolio de proyectos para que tengas una mejor nocion de mis conocimientos y aptitudes.";
